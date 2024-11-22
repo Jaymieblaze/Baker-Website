0131b26de91d73124d7adf0e00ff4c41c05ba1ca
Jaymieblaze <jameseyaiaro@gmail.com>
2024-11-22 19:38:53 +0100

    JS- Remove mobile menu on click of nav links

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // SHOW MENU
-const navMenu = document.getElementById('nav-menu');
-const navToggle = document.getElementById('nav-toggle');
-const navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
 
-/* Menu show */
+// Menu show
 if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
 
-/* Menu hidden */
+// Menu hidden
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
 
+// REMOVE MOBILE
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
